094eb16ffe5244af74a8fc5b67f980b419f8e74b
gh2o <git@thegavinli.com>
2012-02-19 12:02:30 -0800

    disable drive motors for tread fix

--- org/team4159/robot/modules/MotorModule.java
+++ org/team4159/robot/modules/MotorModule.java
@@ -8,11 +8,21 @@ import edu.wpi.first.wpilibj.SpeedController;
 
 public class MotorModule extends Module
 {
+	private static final boolean DISABLED = true;
+	
 	private final Jaguar leftMotor = new Jaguar (HWPorts.Digital_Sidecar.PWM.LEFT_MOTOR_JAGUAR);
 	private final Jaguar rightMotor = new Jaguar (HWPorts.Digital_Sidecar.PWM.RIGHT_MOTOR_JAGUAR);
 	
 	private final SpeedController wrappedLeftMotor = Entry.DEBUG ? new PrintSpeedController (leftMotor, "LEFT") : null;
 	private final SpeedController wrappedRightMotor = Entry.DEBUG ? new PrintSpeedController (rightMotor, "RIGHT") : null;
+	
+	private final SpeedController nullMotor = new SpeedController () {
+		public void pidWrite (double output) {}
+		public double get () { return 0; }
+		public void set (double speed, byte syncGroup) {}
+		public void set (double speed) {}
+		public void disable () {}
+	};
 	
 	private MotorModule ()
 	{
@@ -23,11 +33,15 @@ public class MotorModule extends Module
 	
 	public SpeedController getLeftMotor ()
 	{
+		if (DISABLED)
+			return nullMotor;
 		return Entry.DEBUG ? wrappedLeftMotor : leftMotor;
 	}
 	
 	public SpeedController getRightMotor ()
 	{
+		if (DISABLED)
+			return nullMotor;
 		return Entry.DEBUG ? wrappedRightMotor : rightMotor;
 	}
 	
